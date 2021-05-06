b8d073a175e3adc9bfd5fa9807a8ed9e6d31e3cc
jess <jess.monter@ciencias.unam.mx>
2021-05-05 22:19:08 -0500

    add menu, meals templates for detail, list, create and update

--- backend_test/meals/urls.py
+++ backend_test/meals/urls.py
@@ -2,7 +2,18 @@ from django.conf.urls import url, include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import MealViewSet, MenuViewSet
+from .views import (
+    MealViewSet,
+    MenuViewSet,
+    MenuDetailView,
+    MenuCreateView,
+    MenuListView,
+    MenuUpdateView,
+    MealDetailView,
+    MealCreateView,
+    MealListView,
+    MealUpdateView,
+)
 
 router = DefaultRouter()
 router.register(r"menus", MenuViewSet, basename="menu")
@@ -10,4 +21,12 @@ router.register(r"meals", MealViewSet, basename="meal")
 
 urlpatterns = [
     url(r"^", include(router.urls)),
+    path("menu/<uuid:uuid>", MenuDetailView.as_view(), name="menu-detail"),
+    path("menu/<uuid:uuid>/update", MenuUpdateView.as_view(), name="menu-update"),
+    path("menu", MenuListView.as_view(), name="menu-list"),
+    path("menu/new", MenuCreateView.as_view(), name="menu-create"),
+    path("meal/<int:pk>", MealDetailView.as_view(), name="meal-detail"),
+    path("meal/<int:pk>/update", MealUpdateView.as_view(), name="meal-update"),
+    path("meal", MealListView.as_view(), name="meal-list"),
+    path("meal/new", MealCreateView.as_view(), name="meal-create"),
 ]
--- backend_test/meals/views.py
+++ backend_test/meals/views.py
@@ -1,4 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from django.utils import timezone
 from .models import Meal, Menu
 from .serializers import MealSerializer, MenuSerializer
 
@@ -15,3 +20,83 @@ class MenuViewSet(ModelViewSet):
 
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
+class MenuDetailView(DetailView):
+
+    model = Menu
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+
+    def get_object(self, queryset=None):
+        return Menu.objects.get(id=self.kwargs.get("uuid"))
+
+
+class MenuCreateView(CreateView):
+
+    model = Menu
+    fields = ["available_on", "meals"]
+    success_url = reverse_lazy("menu-list")
+
+
+class MenuListView(ListView):
+
+    model = Menu
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+
+
+class MenuUpdateView(UpdateView):
+    model = Menu
+    fields = ["available_on", "meals"]
+    template_name_suffix = "_update_form"
+    success_url = reverse_lazy("menu-list")
+
+    def get_object(self, queryset=None):
+        return Menu.objects.get(id=self.kwargs.get("uuid"))
+
+
+class MealDetailView(DetailView):
+
+    model = Meal
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+
+    def get_object(self, queryset=None):
+        return Meal.objects.get(pk=self.kwargs.get("pk"))
+
+
+class MealCreateView(CreateView):
+
+    model = Meal
+    fields = ["dishes"]
+    success_url = reverse_lazy("meal-list")
+
+
+class MealListView(ListView):
+
+    model = Meal
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+
+
+class MealUpdateView(UpdateView):
+    model = Meal
+    fields = ["dishes"]
+    template_name_suffix = "_update_form"
+    success_url = reverse_lazy("meal-list")
+
+    def get_object(self, queryset=None):
+        return Meal.objects.get(pk=self.kwargs.get("pk"))
